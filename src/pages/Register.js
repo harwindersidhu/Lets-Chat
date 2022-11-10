6eb4dc2b9df952e26bf1f7efda4cf2db91757fb9
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-11-09 23:13:42 -0700

    Add register page

---
+++ src/pages/Register.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Add from "../img/addAvatar.png";
+
+const Register = () => {
+  return (
+    <div className="form-container">
+      <div className="form-wrapper">
+        <span className="logo">Chat App</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input type="file" id="file" style={{display: "none"}} />
+          <label htmlFor="file">
+            <img src={Add} />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>Do you have an account? Login</p>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
