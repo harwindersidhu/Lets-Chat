958d3881671337b7a23ea625a6369c0984a84d8a
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-11-10 17:15:08 -0700

    Add login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./style.scss"
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 }
