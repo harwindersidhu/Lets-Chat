0b9fcd6b629a359ab91d40bdab670a6744d2aacf
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-11-12 00:42:24 -0700

    Add searchbar to sidebar

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,17 @@ import React from "react";
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className="search">
+      <div className="search-form">
+        <input type="text" placeholder="find a user" />
+      </div>
+      <div className="user-chat">
+        <img src="https://images.pexels.com/photos/1366630/pexels-photo-1366630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className="userChatInfo">
+          <span>Dad</span>
+        </div>
+      </div>
+    </div>
   );
 }
 
