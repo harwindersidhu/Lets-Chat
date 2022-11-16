1cba81532fc6f1022e5aca9686f81a770a425bca
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-11-15 17:31:48 -0700

    Style the message considering owner send the message

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="message-info">
         <img src="https://images.pexels.com/photos/103123/pexels-photo-103123.jpeg?auto=compress&cs=tinysrgb&w=600"
           alt=""
@@ -11,7 +11,9 @@ const Message = () => {
       </div>
       <div className="message-content">
         <p>hello</p>
-        {/* <img src="https://images.pexels.com/photos/103123/pexels-photo-103123.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" /> */}
+        <img src="https://images.pexels.com/photos/103123/pexels-photo-103123.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
       </div>
     </div>
   );
