1cee6d4fd6daf59f2a2187008ee6a927f1511fcd
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-11-24 17:23:42 -0700

    firebase sign in with email and password

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,41 @@
-import React from "react";
+import { signInWithEmailAndPassword } from "@firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import { auth } from "../firebase";
 
 const Login = () => {
+
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      console.log("Error while registering: ", error);
+      setError(true);
+    }
+
+  }
+
   return (
     <div className="form-container">
       <div className="form-wrapper">
         <span className="logo">Chat App</span>
         <span className="title">Login</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
           <button>Sign In</button>
+          {error && <span>Something went wrong.</span>}
         </form>
-        <p>You don't have an account? Register</p>
+        <p>You don't have an account? <Link to="/register">Register</Link></p>
       </div>
     </div>
   );
