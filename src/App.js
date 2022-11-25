abfdef2732a653d00f4c6fb1abda054231f165d1
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-11-24 17:12:23 -0700

    React route protected route

--- src/App.js
+++ src/App.js
@@ -5,21 +5,32 @@ import "./style.scss";
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
+  Navigate
 } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 
 function App() {
 
-  const {currentUser} = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
   console.log("CurrentUserr: ", currentUser);
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    }
+  }
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            } />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
