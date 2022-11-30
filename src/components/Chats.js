703f3430c59449cdb9f886fd5d10156fcee0efef
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-11-29 20:31:36 -0700

    Fetch realtime chats from firebase database and display them on sidebar

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,36 +1,43 @@
-import React from "react";
+import { doc, onSnapshot } from "@firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { db } from "../firebase";
 
 const Chats = () => {
+
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      //onSnapshot: firebase function to fetch realtime updates
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  //Response from getChats() was in object form. SO we will convert it into array.
+  console.log("Chats: ", Object.entries(chats));
+  const userChats = Object.entries(chats)?.map((chat) => {
+    return <div className="user-chat" key={chat[0]}>
+      <img src={chat[1].userInfo.photoURL} alt="" />
+      <div className="user-chat-info">
+        <span>{chat[1].userInfo.displayName}</span>
+        <p>{chat[1].lastMessage?.text}</p>
+      </div>
+    </div>
+  });
+
   return (
     <div className="chats">
-      <div className="user-chat">
-        <img src="https://images.pexels.com/photos/1366630/pexels-photo-1366630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-        <div className="user-chat-info">
-          <span>Dad</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="user-chat">
-        <img src="https://images.pexels.com/photos/1366630/pexels-photo-1366630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-        <div className="user-chat-info">
-          <span>Mom</span>
-          <p>Good night</p>
-        </div>
-      </div>
-      <div className="user-chat">
-        <img src="https://images.pexels.com/photos/1366630/pexels-photo-1366630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-        <div className="user-chat-info">
-          <span>Brother</span>
-          <p>Where are you?</p>
-        </div>
-      </div>
-      <div className="user-chat">
-        <img src="https://images.pexels.com/photos/1366630/pexels-photo-1366630.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-        <div className="user-chat-info">
-          <span>Aunt</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      {userChats}
     </div>
   );
 }
