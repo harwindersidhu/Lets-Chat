85b557d204336d733bdde34d6430ee929cba8e16
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-11-30 23:04:31 -0700

    Create ChatContext to get chat and user info of selected user from sidebar

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import Cam from "../img/cam.png";
 import Add from "../img/add.png";
 import More from "../img/more.png";
 import Messages from "./Messages";
 import Input from "./Input"
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
+  const { userData } = useContext(ChatContext);
   return (
     <div className="chat">
       <div className="chat-info">
-        <span>Jane</span>
+        <span>{userData.user?.displayName}</span>
         <div className="chat-icons">
           <img src={Cam} alt="" />
           <img src={Add} alt="" />
--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,12 +1,14 @@
 import { doc, onSnapshot } from "@firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 import { db } from "../firebase";
 
 const Chats = () => {
 
   const [chats, setChats] = useState([]);
   const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
   useEffect(() => {
     const getChats = () => {
@@ -23,10 +25,14 @@ const Chats = () => {
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
+  const handleSelect = (user) => {
+    dispatch({type: "CHANGE_USER", payload:user})
+  };
+
   //Response from getChats() was in object form. SO we will convert it into array.
   console.log("Chats: ", Object.entries(chats));
   const userChats = Object.entries(chats)?.map((chat) => {
-    return <div className="user-chat" key={chat[0]}>
+    return <div className="user-chat" key={chat[0]} onClick={() => handleSelect(chat[1].userInfo)} >
       <img src={chat[1].userInfo.photoURL} alt="" />
       <div className="user-chat-info">
         <span>{chat[1].userInfo.displayName}</span>
